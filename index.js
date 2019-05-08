e44829624bc7b4dee9fb089b55c70845611811e8
Ryan Bravo <rrbravo2@gmail.com>
2019-05-08 18:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function nestedTarget() {
 }
 
 function increseRankBy(n) {
-  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const list = document.querySelectorAll('ul.ranked-list li');
   
-  for(var i = 0; i < list.length; i++) {
+  for( var i = 0; i < list.length; i++) {
     list[i].innerHTML = parseInt(list[i].innerHTML) + n
   }
 }
