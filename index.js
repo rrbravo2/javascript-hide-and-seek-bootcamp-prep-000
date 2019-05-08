ea31b2342138bbf325091f1c3a4335af45be7a24
Ryan Bravo <rrbravo2@gmail.com>
2019-05-08 18:04:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('ul.ranked-list li');
+  
+  for( var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+}
+
+
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node')
+  let nextNode = grandNode.children[0];
+  
+  while (nextNode) {
+    grandNode = nextNode;
+    nextNode = grandNode.children[0];
+  }
+  return grandNode
+}
